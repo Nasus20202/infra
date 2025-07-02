9e4611caed007e8aa26ef7a1a78103942be2beec
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-07-02 17:40:05 +0200

    chore: change volume backup policy

--- terraform/oci/compute/backup.tf
+++ terraform/oci/compute/backup.tf
@@ -1,18 +1,9 @@
-resource "oci_core_boot_volume_backup" "arm_backup" {
-  count = length(oci_core_instance.arm)
-  boot_volume_id = oci_core_instance.arm[count.index].boot_volume_id
-
-  compartment_id = oci_identity_compartment.compartment.id
-  display_name   = "${local.prefix}-arm-${count.index}-backup"
-  freeform_tags  = local.tags
-}
-
 resource "oci_core_volume_backup_policy" "arm_backup_policy" {
   # Weekly backup with 2 weeks retention
   schedules {
     backup_type = "INCREMENTAL"
     period = "ONE_WEEK"
-    retention_seconds = 14 * 24 * 60 * 60
+    retention_seconds = 13.5 * 24 * 60 * 60 # Delete the backup before creating a new one to avoid billing issues
     hour_of_day = 0
     day_of_week = "SUNDAY"
   }
@@ -23,7 +14,7 @@ resource "oci_core_volume_backup_policy" "arm_backup_policy" {
 }
 
 resource "oci_core_volume_backup_policy_assignment" "arm_backup_policy_assignment" {
-  count = length(oci_core_boot_volume_backup.arm_backup)
+  count = length(oci_core_instance.arm)
   
   asset_id = oci_core_instance.arm[count.index].boot_volume_id
   policy_id = oci_core_volume_backup_policy.arm_backup_policy.id
