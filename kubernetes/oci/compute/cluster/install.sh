82bcfdcd29d3cadc29157eb6c7493225f6a57b9d
Krzysztof Nasuta <krzysztof@nasuta.dev>
2026-02-03 10:55:32 +0100

    refactor: change argo-cd manifests structure

--- kubernetes/oci/compute/cluster/install.sh
+++ kubernetes/oci/compute/cluster/install.sh
@@ -28,5 +28,6 @@ echo "Installing ArgoCD"
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
-helm install argocd argo/argo-cd -f argocd/values.yaml --create-namespace --namespace argocd --atomic
-kubectl apply -f argocd-apps/argocd-infra-root-app.yaml
+helm install argocd argo/argo-cd -f applications/argocd/values.yaml --create-namespace --namespace argocd --atomic
+kubectl apply -f applications/argocd/manifests/projects -n argocd
+kubectl apply -f argocd-apps/argocd-infra-root-app.yaml -n argocd
